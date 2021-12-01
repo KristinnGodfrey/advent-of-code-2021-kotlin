e6cdc64a27ce5ad2c280f9889072054489018272
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-01 23:41:26 +0000

    feat: day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        var counter = 1
+        for (i in 1 until input.size) {
+            if (input[i] > input[i - 1]) {
+                counter++
+            }
+        }
+        return counter
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        var counter = 1
+        var oldSum: Int = input[0] + input[1] + input[2]
+        for (i in 1 until (input.size - 3)) {
+            val newSum: Int = input[i] + input[i + 1] + input[i + 2]
+            if (newSum > oldSum) {
+                counter++
+            }
+            oldSum = newSum
+        }
+        return counter
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput: List<Int> = readInput("resources/day1")
+        .map(String::toInt)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    // iterate through testInput
+    println("Answer 1: ${part1(testInput)}")
+    println("Answer 2: ${part2(testInput)}")
+
+//    val input = readInput("Day01")
+//    println(part1(input))
+//    println(part2(input))
 }
