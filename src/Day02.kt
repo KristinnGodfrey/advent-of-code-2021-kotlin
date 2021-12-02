01c74472ee3de2746d870d5bbcec257a2c548a71
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-02 23:09:39 +0000

    feat: day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,37 +1,49 @@
+import java.io.File
+
 fun main() {
-    fun part1(input: List<String>): Int {
+    fun part1(input: List<Commands>): Int {
         var horizontalPosition: Int = 0
         var verticalPosition: Int = 0
-        for (line in input) {
-            when (line.substringBeforeLast(" ")) {
-                "forward" -> horizontalPosition += line.substringAfterLast(" ").toInt()
-                "up" -> verticalPosition -= line.substringAfterLast(" ").toInt()
-                "down" -> verticalPosition += line.substringAfterLast(" ").toInt()
+
+        for ((keyword, value) in input) {
+            when (keyword) {
+                "forward" -> horizontalPosition += value
+                "up" -> verticalPosition -= value
+                "down" -> verticalPosition += value
             }
         }
+
         return horizontalPosition * verticalPosition
     }
 
-    fun part2(input: List<String>): Int {
+    fun part2(input: List<Commands>): Int {
         var horizontalPosition: Int = 0
         var verticalPosition: Int = 0
         var aim: Int = 0
-        for (line in input) {
-            when (line.substringBeforeLast(" ")) {
-                "forward" ->
-                    horizontalPosition += line.substringAfterLast(" ").toInt().also {
-                        verticalPosition += aim * line.substringAfterLast(" ").toInt()
-                    }
-                "up" -> aim -= line.substringAfterLast(" ").toInt()
-                "down" -> aim += line.substringAfterLast(" ").toInt()
+
+        for ((keyword, value) in input) {
+            when (keyword) {
+                "forward" -> {
+                    horizontalPosition += value
+                    verticalPosition += aim * value
+                }
+                "up" -> aim -= value
+                "down" -> aim += value
             }
         }
+
         return horizontalPosition * verticalPosition
     }
 
-    val input: List<String> = readInput("resources/day2")
+    val input = File("src/resources/day2.txt")
+        .readLines()
+        .map {
+            val (command, value) = it.split(" ")
+            Commands(command, value.toInt())
+        }
 
     println("Answer 1: ${part1(input)}")
     println("Answer 2: ${part2(input)}")
 }
 
+data class Commands(val command: String, val value: Int)
