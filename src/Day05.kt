8f208067314c9467f607ad0787e188b993232660
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-05 19:44:59 +0000

    wip: day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,39 +1,50 @@
 import java.io.File
 
-data class InputPoints(val x: Int, val y: Int)
-data class InputPointsString(val x: String, val y: String)
+data class InputPointsString(val x: Pair<Int, Int>, val y: Pair<Int, Int>)
+data class GridWithPoints(var grid: MutableList<List<Int>>, var points: Int)
 
 fun main() {
-    fun create10x10Grid(): MutableList<Triple<Int, Int, Int>> {
-        var listOfGridItems: MutableList<Triple<Int, Int, Int>> = mutableListOf()
+    fun create10x10Grid(): MutableList<List<Int>> {
+        val gridWithPoints: GridWithPoints
+
+
         for (i in 0..9) {
             for (j in 0..9) {
-                listOfGridItems.add(Triple(i, j, 0))
+                gridWithPoints.grid.add(i, listOf(j))
+//                    .add(listOf(i, j)), 0)
             }
         }
-        return listOfGridItems
-    }
+        return grid
 
-    fun createInputPoints(input: List<InputPointsString>): MutableList<InputPoints> {
-        val listOfInputPoints: MutableList<InputPoints> = mutableListOf()
-
-        return listOfInputPoints
     }
 
     fun part1(input: List<InputPointsString>): Int {
-        var grid = create10x10Grid()
+        val grid = create10x10Grid()
+        for (line in input) {
+            val xFirst = line.x.first
+            val yFirst = line.y.first
+            val xSecond = line.x.second
+            val ySecond = line.y.second
+            println(
+                "xFirst: $xFirst, xSecond: $xSecond \nyFirst: $yFirst, xSecond: $ySecond"
+            )
 
-        println(input)
+            if (xFirst > xSecond) {
+                println("WASUP")
+                for (i in xSecond..xFirst) {
+                    grid[x]
+                    println(i)
+                }
 
+            } else if (xFirst > xSecond) {
 
-//        val listOfInputPoints = createInputPoints(input)
+            } else if (xSecond > xSecond) {
 
-        for (i in 0 until grid.size) {
-            if (grid[i].first == 0 && grid[i].second == 0) {
-                grid[i] = grid[i].copy(third = 3)
+            } else if (xSecond < xSecond) {
+
             }
+            println()
         }
-
         return 0
     }
 
@@ -44,9 +55,16 @@ fun main() {
     val input = File("src/resources/day5Tester.txt")
         .readLines()
         .map {
-            val (left, right) = it.split("-> ")
-            InputPointsString(left, right)
+            val (left, right) = it.split(" -> ")
+            val (x, y) = left.split(",")
+            val (z, t) = right.split(",")
+            InputPointsString(Pair(x.toInt(), y.toInt()), Pair(z.toInt(), t.toInt()))
+
         }
+
+    println(input)
+
+    println("input 0: ${input[0].x.first}, ${input[0].x.second}")
 
     println("Answer 1: ${part1(input)}")
     println("Answer 2: ${part2(input)}")
