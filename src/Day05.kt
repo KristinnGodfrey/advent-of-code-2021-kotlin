b63992998a694dfd6b1510a45df35bc1c858a905
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-05 22:13:53 +0000

    wip: day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -7,9 +7,9 @@ fun main() {
     fun create10x10Grid(input: List<InputPointsString>): Array<IntArray> {
         val mx = input.maxOf { l -> maxOf(l.x.first, l.x.second) }
         val my = input.maxOf { l -> maxOf(l.y.first, l.y.second) }
-        val grid = Array(mx + 1) { IntArray(mx + 1) }
+        val grid = Array(mx + 1) { IntArray(my + 1) }
         for (i in input.indices) {
-            val row = IntArray(10)
+            val row = IntArray(mx + 1)
             for (j in row.indices) {
                 row[j] = 0
             }
@@ -26,18 +26,18 @@ fun main() {
 
         for (line in input) {
             val x1 = line.x.first
-            val x2 = line.x.second
-            val y1 = line.y.first
+            val y1 = line.x.second
+            val x2 = line.y.first
             val y2 = line.y.second
             println("x1: $x1, x2: $x2")
             println("y1: $y1, y2: $y2")
 
-            if (x1 == y1) {
+            if (x1 == x2) {
                 println("up")
                 for (i in minOf(y1, y2)..maxOf(y1, y2)) {
                     grid[x1][i]++
                 }
-            } else if (x2 == y2) {
+            } else if (y1 == y2) {
                 println("down")
                 for (i in minOf(x1, x2)..maxOf(x1, x2)) {
                     grid[i][y1]++
@@ -46,17 +46,23 @@ fun main() {
                 println("neither")
             }
 
-            for (array in grid) {
-                for (value in array) {
-                    print(value)
-                    print(" ")
-                }
-                println()
-            }
-
             println()
         }
-        return 0
+
+        var highest = 0
+        for (array in grid) {
+            for (value in array) {
+                print(value)
+                print(" ")
+                if (value > highest) {
+                    highest = value
+                }
+            }
+            println()
+        }
+        return highest
+
+
     }
 
     fun part2(input: List<InputPointsString>): Int {
@@ -67,9 +73,9 @@ fun main() {
         .readLines()
         .map {
             val (left, right) = it.split(" -> ")
-            val (x, y) = left.split(",")
-            val (z, t) = right.split(",")
-            InputPointsString(Pair(x.toInt(), y.toInt()), Pair(z.toInt(), t.toInt()))
+            val (x1, y1) = left.split(",").map { it.toInt() }
+            val (x2, y2) = right.split(",").map { it.toInt() }
+            InputPointsString(Pair(x1, y1), Pair(x2, y2))
         }
 
     println("Answer 1: ${part1(input)}")
