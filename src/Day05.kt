dd969b200a5f6feaad799d7ae443de6c2e97c0d2
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-06 00:24:27 +0000

    wip: day 5

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,48 +1,30 @@
 import java.io.File
 
 
-data class InputPointsString(val x: Pair<Int, Int>, val y: Pair<Int, Int>)
+data class InputPointsString(val x1: Int, val y1: Int, val x2: Int, val y2: Int)
 
 fun main() {
-    fun create10x10Grid(input: List<InputPointsString>): Array<IntArray> {
-        val mx = input.maxOf { l -> maxOf(l.x.first, l.x.second) }
-        val my = input.maxOf { l -> maxOf(l.y.first, l.y.second) }
-        val grid = Array(mx + 1) { IntArray(my + 1) }
-        for (i in input.indices) {
-            val row = IntArray(mx + 1)
-            for (j in row.indices) {
-                row[j] = 0
-            }
-            grid[i] = row
-        }
-        return grid
-    }
 
     fun part1(input: List<InputPointsString>): Int {
-//        val grid = create10x10Grid(input)
-        val mx = input.maxOf { l -> maxOf(l.x.first, l.x.second) }
-        val my = input.maxOf { l -> maxOf(l.y.first, l.y.second) }
+        val mx = input.maxOf { l -> maxOf(l.x1, l.x2) }
+        val my = input.maxOf { l -> maxOf(l.y1, l.y2) }
         val grid = Array(mx + 1) { IntArray(my + 1) }
 
-        for (line in input) {
-            val x1 = line.x.first
-            val y1 = line.x.second
-            val x2 = line.y.first
-            val y2 = line.y.second
-            println("x1: $x1, x2: $x2")
-            println("y1: $y1, y2: $y2")
+        for (l in input) {
+            println("x1: ${l.x1}, x2: ${l.x2}")
+            println("y1: ${l.y1}, y2: ${l.y2}")
 
-            if (x1 == x2) {
+            if (l.x1 == l.x2) {
                 println("up")
-                for (i in minOf(y1, y2)..maxOf(y1, y2)) {
+                for (i in minOf(l.y1, l.y2)..maxOf(l.y1, l.y2)) {
                     print("$i ")
-                    grid[i][y1]++
+                    grid[i][l.y1]++
                 }
-            } else if (y1 == y2) {
+            } else if (l.y1 == l.y2) {
                 println("down")
-                for (i in minOf(x1, x2)..maxOf(x1, x2)) {
+                for (i in minOf(l.x1, l.x2)..maxOf(l.x1, l.x2)) {
                     print("$i ")
-                    grid[x1][i]++
+                    grid[l.x1][i]++
                 }
             } else {
                 println("neither")
@@ -73,11 +55,11 @@ fun main() {
 
     val input = File("src/resources/day5Tester.txt")
         .readLines()
-        .map {
-            val (left, right) = it.split(" -> ")
-            val (x1, y1) = left.split(",").map { it.toInt() }
-            val (x2, y2) = right.split(",").map { it.toInt() }
-            InputPointsString(Pair(x1, y1), Pair(x2, y2))
+        .map { l ->
+            val s = l.split(" -> ")
+            val (x1, y1) = s[0].split(",").map { it.toInt() }
+            val (x2, y2) = s[1].split(",").map { it.toInt() }
+            InputPointsString(x1, y1, x2, y2)
         }
 
     println("Answer 1: ${part1(input)}")
