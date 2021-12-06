2427eca0c61bcc2b05f5f63e66367b9d24cfd24d
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-06 23:12:44 +0000

    feat: day 6

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,61 +1,46 @@
 import java.io.File
 
-fun insertionsort(items: MutableList<Int>): MutableList<Int> {
-    if (items.isEmpty() || items.size < 2) {
-        return items
-    }
-    for (count in 1..items.count() - 1) {
-        // println(items)
-        val item = items[count]
-        var i = count
-        while (i > 0 && item < items[i - 1]) {
-            items[i] = items[i - 1]
-            i -= 1
-        }
-        items[i] = item
-    }
-    return items
-}
-
-fun grow(n: Int): List<Int> =
-    if (n == 0) {
-        listOf(6, 8)
-    } else {
-        listOf(n - 1)
-    }
-
 fun main() {
     fun part1(input: MutableList<Int>): Int {
-        var l_input = insertionsort(input)
-        var listOfOnes: List<Int> = listOf()
-        var listOfTwos: List<Int> = listOf()
-        var listOfThrees: List<Int> = listOf()
-        var listOfFours: List<Int> = listOf()
-        var listOfFives: List<Int> = listOf()
-        var listOfSixes: List<Int> = listOf()
-        println(l_input)
-        for (i in 0 until 5) {
-            for (j in 0 until l_input.size)
-                if (l_input[j] == 0) {
-                    l_input[j] = 6 + 1
-                    l_input.add(8 + 1)
+        var mutableInput = input
+        for (i in 0 until 80) {
+            for (j in 0 until mutableInput.size) {
+                println(mutableInput.size)
+                if (mutableInput[j] == 0) {
+                    mutableInput[j] = 6 + 1
+                    mutableInput.add(8 + 1)
                 }
-            l_input = l_input.map { it - 1 } as MutableList<Int>
-            println(l_input)
-
+            }
+            mutableInput = mutableInput.map { it - 1 } as MutableList<Int>
         }
-        return l_input.size
+        return mutableInput.size
     }
 
-    var input: MutableList<Int> = File("src/resources/day6.txt")
+    fun part2(input: MutableList<Int>): Long {
+        val fishArray = LongArray(9)
+
+        input.forEach { l -> fishArray[l] = fishArray[l] + 1 }
+
+        for (i in 0 until 256) {
+            val depletedFishes = fishArray[0]
+
+            for (j in 1 until fishArray.size) {
+                fishArray[j - 1] = fishArray[j]
+            }
+            fishArray[fishArray.size - 1] = 0
+
+            fishArray[6] += depletedFishes
+            fishArray[8] += depletedFishes
+        }
+
+        return fishArray.sum()
+    }
+
+    val input: MutableList<Int> = File("src/resources/day6.txt")
         .readLines()[0]
         .split(",")
         .map { it.toInt() } as MutableList<Int>
 
-    // read file and convert to list of ints
-
-
-    // println(input)
-    // println(input[0] + input[1])
     println("Answer 1: ${part1(input)}")
+    println("Answer 2: ${part2(input)}")
 }
