c9c601c11a98ccdc917ff3dba8bff3312a5e1ea4
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-11 03:04:45 +0000

    wip 10

--- src/Day10.kt
+++ src/Day10.kt
@@ -6,38 +6,47 @@ fun main() {
     val inputStringBuilder = StringBuilder()
     File("src/resources/day10.txt")
         .readLines()
-        .forEach { i -> inputStringBuilder.append(i) }
+        .forEach { i -> inputStringBuilder.append("$i ") }
 
     val inputString = inputStringBuilder
-        .replace(".".toRegex(), "$0 ") // inputString with spaces
-
-    val input = inputString
         .trim()
         .split(" ")
-        .map { it.single() }
 
+//    val input = inputString
+//        .trim()
+//        .split(" ")
+////        .map { it.single() }
+
+    println(inputString)
+
+//
+//    val charArray: List<List<Char>> =
+//        listOf(input.map { it. })
+
+//    println(charArray[0])
+
+//    val inputChar: List<Char> = input
+//        .forEach { it.forEach { l -> l.toChar() } }
 
     val putters: List<Char> = listOf('(', '[', '{', '<')
     val poppers: List<Char> = listOf(')', ']', '}', '<')
     val stack = ArrayDeque<Char>()
 
-//    println(input)
-
-    for (i in input) {
-        var counter = 0
-        if (i in putters) {
-            stack.add(i)
-        } else if (i in poppers) {
-            val lastIndex = getLastIndex(stack, putters)
-            val currentIndex = getCurrentIndex(stack, poppers)
-            println("${stack.last()}: $lastIndex")
-            println("$i: $currentIndex")
-            println()
-        }
-        counter++
-        println("iteration: $counter")
-    }
-    println(stack)
+//    for (i in input) {
+//        var counter = 0
+//        if (i in putters) {
+//            stack.add(i)
+//        } else if (i in poppers) {
+//            val lastIndex = getLastIndex(stack, putters)
+//            val currentIndex = getCurrentIndex(stack, poppers)
+//            println("${stack.last()}: $lastIndex")
+//            println("$i: $currentIndex")
+//            println()
+//        }
+//        counter++
+//        println("iteration: $counter")
+//    }
+//    println(stack)
 }
 
 fun getLastIndex(stack: ArrayDeque<Char>, putters: List<Char>): Int {
