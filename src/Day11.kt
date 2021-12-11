350d6839df28b5f4e4973cd78928e796859f1bb7
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-11 18:07:06 +0000

    wip day 11

--- src/Day11.kt
+++ src/Day11.kt
@@ -4,10 +4,15 @@ fun main() {
 
     val input = File("src/resources/day11.txt")
         .readLines()
-        .map { it.map { l -> l - '0' }.toIntArray() }
-    println(input)
-    println(input[0])
-    for (i in input[0].indices) {
-        println(input[0][i])
+        .map { it.map { l -> l - '0' }.toMutableList() }.toMutableList()
+
+    input.forEachIndexed { li, e ->
+        println(input[e[0]])
+        e.forEachIndexed { i, v ->
+//            println(v)
+        }
+        println("")
     }
+
+
 }
