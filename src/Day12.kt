ffeabdad5098ed2d9be73b403c90c29795acd0bb
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-12 21:14:30 +0000

    wip

--- src/Day12.kt
+++ src/Day12.kt
@@ -7,8 +7,6 @@ fun main() {
             .readLines()
             .map { it.split("-") }
 
-//        println(input)
-
         val smallCaves: MutableList<String> = mutableListOf()
         val bigCaves: MutableList<String> = mutableListOf()
 
@@ -27,12 +25,6 @@ fun main() {
             }
         }
 
-//        val distinct = input.toSet().toList()
-//        println("distinct $distinct")
-
-//        println(smallCaves)
-//        println(bigCaves)
-
         fun smallCaveInList(current: List<String>, paths: MutableList<String>): Boolean {
             if (current[1] in smallCaves && current[1] in paths) {
                 return true
@@ -42,42 +34,29 @@ fun main() {
 
         fun findPaths(current: List<String>, lastArg: List<String>, paths: MutableList<String>): MutableList<String> {
             var last = lastArg
-//            println("current: ${current[1]}")
-//            input.forEach { println("IT: ${it[1]}") }
 
-//            paths.add(current[1])
             paths.add(current[1])
+            println("current: $current")
             val nextList = input.filter { it[0] == current[1] }.toMutableList()
-            var flag = false
             while (nextList.isNotEmpty()) {
-                if (!flag) {
-                    println(nextList)
-                    flag = true
-                }
                 val next = nextList.removeFirst()
-                println("last: $last")
-                println("current: $current")
-                println("next: $next")
                 if (next[1] == "end") {
                     paths.add("end")
                 } else if (!smallCaveInList(next, paths)) {
-                    last = current
                     findPaths(next, current, paths)
+                } else if (current[0] in bigCaves) {
+                    println("lol")
+                    findPaths(last, current, paths)
                 }
-//                else if (current[1] in bigCaves) {
-//                    println("lol")
-//                    findPaths(next, last, paths)
-//                }
             }
             println("---")
-            return mutableListOf()
+            return mutableListOf("")
         }
 
         input.forEachIndexed { i, j ->
             val paths = mutableListOf<String>("start")
 //            println(findPaths(j, paths))
             findPaths(j, listOf(""), paths)
-
         }
 
 
