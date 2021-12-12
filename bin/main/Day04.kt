7fd5adf502d722553e1fc1cc16a615aa31ad0f5b
KristinnGodfrey <kristinngodfrey@gmail.com>
2021-12-12 05:02:31 +0000

    feat day 4-1

--- bin/main/Day04.kt
+++ bin/main/Day04.kt
@@ -3,8 +3,6 @@ data class Player(var board: MutableList<MutableList<Int>>, var hasWon: Boolean)
 fun main() {
 
     fun calculateWinningScore(player: Player, value: Int): Int {
-        println("We have a winner!")
-        println(player)
         val scoreMap = player.board.flatMap {
             it.filter { l -> l != 999 }
         }
@@ -15,8 +13,6 @@ fun main() {
         val b = player.board
 
         if (b[y].all { it == 999 }) {
-            println("horizontal")
-            println(b[y])
             return true
         }
         var counter = 0
@@ -24,7 +20,6 @@ fun main() {
             if ((b[i][x]) == 999) counter++
         }
         if (counter == 5) {
-            println("vertical")
             return true
         }
         return false
@@ -33,7 +28,7 @@ fun main() {
     fun findValueInBoard(player: Player, value: Int): Int {
         player.board.forEachIndexed { y, mutableList ->
             mutableList.forEachIndexed { x, v ->
-                if ((value == v) and (!player.hasWon)) {
+                if ((value == v)) {
                     player.board[y][x] = 999
                     if (checkWinner(player, y, x)) {
                         player.hasWon = true
@@ -61,7 +56,32 @@ fun main() {
             .chunked(5)
             .forEach { listOfPlayers.add(Player(it as MutableList<MutableList<Int>>, false)) }
 
-        println("I: XX, $listOfPlayers")
+        var ans = 0
+        for (i in inputNumbers) {
+            listOfPlayers.forEach {
+                if (ans == 0) ans += findValueInBoard(it, i)
+                else return ans
+            }
+        }
+        return ans
+    }
+
+
+    fun part2(input: List<String>): Int {
+        val listOfPlayers = mutableListOf<Player>()
+        val inputNumbers = input[0]
+            .split(",")
+            .map { it.toInt() }
+
+        input
+            .slice(1 until input.size)
+            .map { it.replace("  ", " ") }
+            .filter { it.isNotEmpty() }
+            .map { it.trim() }
+            .map { it.split(" ") }
+            .map { it.map { l -> l.toInt() } }
+            .chunked(5)
+            .forEach { listOfPlayers.add(Player(it as MutableList<MutableList<Int>>, false)) }
 
         var ans = 0
         for (i in inputNumbers) {
@@ -69,15 +89,8 @@ fun main() {
                 if (ans == 0) ans += findValueInBoard(it, i)
                 else return ans
             }
-            println("I: $i, $listOfPlayers")
         }
         return ans
-    }
-
-
-    fun part2(input: List<String>): Int {
-
-        return 0
     }
 
     val input: List<String> = readInput("resources/day4Tester")
